055f12d02656c814a9bf426585dcec00c0320e44
Ariel Lopes <ariellopes888@gmail.com>
2025-12-03 23:59:04 -0300

    add ITimeZoneProvider

---
+++ Shared/Shared.Core/Providers/ITimeZoneProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Core.Providers
+{
+    public interface ITimeZoneProvider
+    {
+        DateTime ConvertToUtc(DateTime local, string timeZoneId);
+        DateTime ConvertToLocal(DateTime utc, string timeZoneId);
+        TimeZoneInfo GetTimeZoneInfo(string timeZoneId);
+    }
+
+}
